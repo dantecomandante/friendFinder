869beaf2e7c2f9ef9537506f36d3453d445f01e4
Dante Perez <red.dante888@gmail.com>
2019-07-27 05:09:24 -0700

    installing packages and set up

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-
-const path = require("path");
-const PORT = process.env.PORT || 8080;
-const express = require("express");
-
-const app = express();
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
-
-// ROUTES
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
-app.listen(PORT, function () {
-    console.log("Active on PORT: " + PORT);
-});
+//Did not know how to do this part I installed the packages necessary and set up the other parts as much as I could.
